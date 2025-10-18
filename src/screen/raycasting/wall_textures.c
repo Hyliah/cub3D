b9e802a92a4bb72a84d0514ac5c054ba54d71e28
Hygie <lhygie@gmail.com>
2025-10-19 01:09:18 +0200

    begin of textures

---
+++ src/screen/raycasting/wall_textures.c
@@ -0,0 +1,51 @@
+/*****************************************************************************/
+/*                                                                           */
+/*                                                                           */
+/*                       LES CODEUSES DU DIMANCHE                            */
+/*                               FONT UN                                     */
+/*                        __  _  _  ___  ___  ___                            */
+/*                       / _)( )( )(  ,)(__ )(   \                           */
+/*                      ( (_  )()(  ) ,\ (_ \ ) ) )                          */
+/*                       \__) \__/ (___/(___/(___/                           */
+/*                                                                           */
+/*****************************************************************************/
+
+#include "cub.h"
+
+/*
+
+
+      //texturing calculations
+      int texNum = worldMap[mapX][mapY] - 1; //1 subtracted from it so that texture 0 can be used!
+
+      //calculate value of wallX
+      double wallX; //where exactly the wall was hit
+      if (side == 0) wallX = posY + perpWallDist * rayDirY;
+      else           wallX = posX + perpWallDist * rayDirX;
+      wallX -= floor((wallX));
+
+      //x coordinate on the texture
+      int texX = int(wallX * double(texWidth));
+      if(side == 0 && rayDirX > 0) texX = texWidth - texX - 1;
+      if(side == 1 && rayDirY < 0) texX = texWidth - texX - 1;
+
+
+
+
+    
+        // How much to increase the texture coordinate per screen pixel
+      double step = 1.0 * texHeight / lineHeight;
+      // Starting texture coordinate
+      double texPos = (drawStart - h / 2 + lineHeight / 2) * step;
+      for(int y = drawStart; y<drawEnd; y++)
+      {
+        // Cast the texture coordinate to integer, and mask with (texHeight - 1) in case of overflow
+        int texY = (int)texPos & (texHeight - 1);
+        texPos += step;
+        Uint32 color = texture[texNum][texHeight * texY + texX];
+        //make color darker for y-sides: R, G and B byte each divided through two with a "shift" and an "and"
+        if(side == 1) color = (color >> 1) & 8355711;
+        buffer[y][x] = color;
+      }
+    }
+*/
