f53ec0658cae5bfb51bad3a267595bf898f94cff
Hygie Lichtenthaeler <hlichten@c3r4s2.42lausanne.ch>
2025-10-24 16:54:03 +0200

    leak free baby

--- src/malloc_exit/clean_exit.c
+++ src/malloc_exit/clean_exit.c
@@ -12,19 +12,13 @@
 
 #include "cub.h"
 
-void	free_t_img(t_cub *cub, t_img *img);
+static void	free_t_img(t_cub *cub, t_img *img);
+static void	free_after_game(t_cub *cub);
 
 int	clean_exit(t_cub *cub)
 {	
 	if (cub->game_on == TRUE)
-	{
-		free_t_img(cub, &cub->weapons.img_w1);
-		free_t_img(cub, &cub->weapons.img_w2);
-		free_t_img(cub, &cub->weapons.img_w3);
-		free_t_img(cub, &cub->weapons.img_w4);
-		free_t_img(cub, &cub->weapons.img_w5);
-		free_t_img(cub, &cub->oscreen.img_olay);
-	}
+		free_after_game(cub);
 	free_t_img(cub, &cub->mmap.img_mmap);
 	free_t_img(cub, &cub->mmap.img_player);
 	free_t_img(cub, &cub->graphic.img_e);
@@ -38,9 +32,7 @@ int	clean_exit(t_cub *cub)
 	cub->oscreen.img_pe = NULL;
 	mlx_destroy_image(cub->graphic.mlx_ptr, cub->oscreen.img_si);
 	cub->oscreen.img_si = NULL;
-
 	free_tab(&cub->map.map_tab);
-
 	mlx_destroy_window(cub->graphic.mlx_ptr, cub->graphic.win_ptr);
 	cub->graphic.win_ptr = NULL;
 	// ne compile pas sur mac donc mettre seulement sur le linux
@@ -49,10 +41,62 @@ int	clean_exit(t_cub *cub)
 	exit (EXIT_SUCCESS);
 }
 
-void	free_t_img(t_cub *cub, t_img *img)
+static void	free_t_img(t_cub *cub, t_img *img)
 {
 	mlx_destroy_image(cub->graphic.mlx_ptr, img->img_ptr);
 	img->img_ptr = NULL;
-	//soucis ici
-	//free_ptr((void **)&img->addr_ptr);
+	img->addr_ptr = NULL;
 }
+
+static void	free_after_game(t_cub *cub)
+{
+	free_t_img(cub, &cub->weapons.img_w1);
+	free_t_img(cub, &cub->weapons.img_w2);
+	free_t_img(cub, &cub->weapons.img_w3);
+	free_t_img(cub, &cub->weapons.img_w4);
+	free_t_img(cub, &cub->weapons.img_w5);
+	free_t_img(cub, &cub->oscreen.img_olay);
+}
+// int	clean_exit(t_cub *cub)
+// {	
+// 	if (cub->game_on == TRUE)
+// 	{
+// 		free_t_img(cub, &cub->weapons.img_w1);
+// 		free_t_img(cub, &cub->weapons.img_w2);
+// 		free_t_img(cub, &cub->weapons.img_w3);
+// 		free_t_img(cub, &cub->weapons.img_w4);
+// 		free_t_img(cub, &cub->weapons.img_w5);
+// 		free_t_img(cub, &cub->oscreen.img_olay);
+// 	}
+// 	free_t_img(cub, &cub->mmap.img_mmap);
+// 	free_t_img(cub, &cub->mmap.img_player);
+// 	free_t_img(cub, &cub->graphic.img_e);
+// 	free_t_img(cub, &cub->graphic.img_s);
+// 	free_t_img(cub, &cub->graphic.img_n);
+// 	free_t_img(cub, &cub->graphic.img_w);
+// 	free_t_img(cub, &cub->graphic.img_screen);
+// 	free_img_ptr(cub, &cub->oscreen.img_os);
+// 	free_img_ptr(cub, &cub->oscreen.img_pe);
+// 	free_img_ptr(cub, &cub->oscreen.img_si);
+
+// 	free_tab(&cub->map.map_tab);
+
+// 	mlx_destroy_window(cub->graphic.mlx_ptr, cub->graphic.win_ptr);
+// 	cub->graphic.win_ptr = NULL;
+// 	// ne compile pas sur mac donc mettre seulement sur le linux
+// 	mlx_destroy_display(cub->graphic.mlx_ptr);
+// 	free_ptr(&cub->graphic.mlx_ptr);
+// 	exit (EXIT_SUCCESS);
+// }
+
+// static void	free_t_img(t_cub *cub, t_img *img)
+// {
+// 	free_img_ptr(cub, img->img_ptr);
+// 	img->addr_ptr = NULL;
+// }
+
+// static void	free_img_ptr(t_cub *cub, void *img_ptr)
+// {
+// 	mlx_destroy_image(cub->graphic.mlx_ptr, img_ptr);
+// 	img_ptr = NULL;
+// }
--- src/utils/init.c
+++ src/utils/init.c
@@ -12,39 +12,89 @@
 
 #include "cub.h"
 
+static void	init_setting_map_fps(t_cub *cub);
+static void	init_player_oscreen(t_cub *cub);
+static void	init_t_imgs(t_cub *cub);
 static void	init_t_img(t_img *img);
-static void	init_struct_graphic(t_cub *cub);
-static void	init_struct_map_settings(t_cub *cub);
-static void	init_struct_player(t_cub *cub);
+
+#ifdef __linux__
 
 void	init_struct(t_cub *cub)
 {
+	cub->graphic.s_height = 1080;
+	cub->graphic.s_width = 1920;
+	cub->setting.fd = -1;
+	cub->mmap.mm_show = TRUE;
+	cub->oscreen.is_sirius = TRUE;
+	cub->weapons.s_nb = 1;
+	cub->weapons.up = TRUE;
 	cub->game_on = FALSE;
-	init_struct_graphic(cub);
-	init_struct_map_settings(cub);
-	init_struct_player(cub);
+	init_setting_map_fps(cub);
+	init_player_oscreen(cub);
+	init_t_imgs(cub);
 	frame_time(cub);
 }
 
-static void	init_struct_map_settings(t_cub *cub)
+#else
+
+void	init_struct(t_cub *cub)
 {
-	cub->map.height = 0;
-	cub->map.width = 0;
-	cub->map.map_tab = NULL;
+	cub->graphic.s_height = 540;
+	cub->graphic.s_width = 960;
 	cub->setting.fd = -1;
+	cub->mmap.mm_show = TRUE;
+	cub->oscreen.is_sirius = TRUE;
+	cub->weapons.s_nb = 1;
+	cub->weapons.up = TRUE;
+	cub->game_on = FALSE;
+	init_setting_map_fps(cub);
+	init_player_oscreen(cub);
+	init_t_imgs(cub);
+	frame_time(cub);
+}
+
+#endif
+
+static void	init_setting_map_fps(t_cub *cub)
+{
+	cub->fps.frame_time = 0;
+	cub->fps.old_time = 0;
+	cub->fps.start_time = 0;
+	cub->graphic.mlx_ptr = NULL;
+	cub->graphic.win_ptr = NULL;
+	cub->map.width = 0;
+	cub->map.height = 0;
+	cub->map.map_tab = NULL;
+	cub->mmap.mm_hei = 0;
+	cub->mmap.mm_sqr = 0;
+	cub->mmap.mm_wid = 0;
 	cub->setting.c_color = 0;
 	cub->setting.f_color = 0;
 	cub->setting.start_dir = 0;
+	cub->tex.draw_end = 0;
+	cub->tex.draw_start = 0;
+	cub->tex.step = 0;
+	cub->tex.tex_pos = 0;
+	cub->tex.tex_x = 0;
+	cub->tex.tex_y = 0;
+	cub->tex.wall_x = 0;
 }
 
-static void	init_struct_player(t_cub *cub)
+static void	init_t_imgs(t_cub *cub)
 {
-	cub->player.dir_x = 0;
-	cub->player.dir_y = 0;
-	cub->player.pl_x = 0;
-	cub->player.pl_y = 0;
-	cub->player.pos_x = 0;
-	cub->player.pos_y = 0;
+	init_t_img(&cub->weapons.img_w1);
+	init_t_img(&cub->weapons.img_w2);
+	init_t_img(&cub->weapons.img_w3);
+	init_t_img(&cub->weapons.img_w4);
+	init_t_img(&cub->weapons.img_w5);
+	init_t_img(&cub->oscreen.img_olay);
+	init_t_img(&cub->graphic.img_screen);
+	init_t_img(&cub->graphic.img_e);
+	init_t_img(&cub->graphic.img_s);
+	init_t_img(&cub->graphic.img_w);
+	init_t_img(&cub->graphic.img_n);
+	init_t_img(&cub->mmap.img_mmap);
+	init_t_img(&cub->mmap.img_player);
 	cub->key.k_a = FALSE;
 	cub->key.k_s = FALSE;
 	cub->key.k_d = FALSE;
@@ -56,63 +106,33 @@ static void	init_struct_player(t_cub *cub)
 	cub->key.k_sh = FALSE;
 }
 
-#ifdef __linux__
-
-static void	init_struct_graphic(t_cub *cub)
+static void	init_player_oscreen(t_cub *cub)
 {
-	cub->graphic.s_height = 1080;
-	cub->graphic.s_width = 1920;
-	cub->graphic.mlx_ptr = NULL;
-	cub->graphic.win_ptr = NULL;
-	cub->mmap.mm_show = TRUE;
+	cub->player.angle = 0;
+	cub->player.cam_x = 0;
+	cub->player.cam_y = 0;
+	cub->player.delta_dist_x = 0;
+	cub->player.delta_dist_y = 0;
+	cub->player.dir_x = 0;
+	cub->player.dir_y = 0;
+	cub->player.line_height = 0;
+	cub->player.map_x = 0;
+	cub->player.map_y = 0;
+	cub->player.perp_wall_dist = 0;
+	cub->player.pl_x = 0;
+	cub->player.pl_y = 0;
+	cub->player.pos_x = 0;
+	cub->player.pos_y = 0;
+	cub->player.ray_dir_x = 0;
+	cub->player.ray_dir_y = 0;
+	cub->player.side_dist_x = 0;
+	cub->player.side_dist_y = 0;
+	cub->player.step_x = 0;
+	cub->player.step_y = 0;
 	cub->oscreen.img_os = NULL;
 	cub->oscreen.img_pe = NULL;
 	cub->oscreen.img_si = NULL;
-	cub->oscreen.is_sirius = TRUE;
-	cub->weapons.s_nb = 1;
-	cub->weapons.up = TRUE;
-	init_t_img(&cub->weapons.img_w1);
-	init_t_img(&cub->weapons.img_w2);
-	init_t_img(&cub->weapons.img_w3);
-	init_t_img(&cub->weapons.img_w4);
-	init_t_img(&cub->weapons.img_w5);
-	init_t_img(&cub->oscreen.img_olay);
-	init_t_img(&cub->graphic.img_screen);
-	init_t_img(&cub->mmap.img_mmap);
-	init_t_img(&cub->mmap.img_player);
 }
-
-#else
-
-static void	init_struct_graphic(t_cub *cub)
-{
-	cub->graphic.s_height = 540;
-	cub->graphic.s_width = 960;
-	cub->graphic.mlx_ptr = NULL;
-	cub->graphic.win_ptr = NULL;
-	cub->mmap.mm_show = TRUE;
-	cub->oscreen.img_os = NULL;
-	cub->oscreen.img_pe = NULL;
-	cub->oscreen.img_si = NULL;
-	cub->oscreen.is_sirius = TRUE;
-	cub->weapons.s_nb = 1;
-	cub->weapons.up = TRUE;
-	init_t_img(&cub->weapons.img_w1);
-	init_t_img(&cub->weapons.img_w2);
-	init_t_img(&cub->weapons.img_w3);
-	init_t_img(&cub->weapons.img_w4);
-	init_t_img(&cub->weapons.img_w5);
-	init_t_img(&cub->oscreen.img_olay);
-	init_t_img(&cub->graphic.img_screen);
-	init_t_img(&cub->mmap.img_mmap);
-	init_t_img(&cub->mmap.img_player);
-	init_t_img(&cub->graphic.img_e);
-	init_t_img(&cub->graphic.img_s);
-	init_t_img(&cub->graphic.img_n);
-	init_t_img(&cub->graphic.img_w);
-}
-
-#endif
 
 static void	init_t_img(t_img *img)
 {
